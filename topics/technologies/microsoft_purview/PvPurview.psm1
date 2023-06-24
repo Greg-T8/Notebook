4b58bc38ef5e211607ba92b00cb7961e6605c851
Greg Tate <gregt8@outlook.com>
2023-06-24 02:55:29 -0500

    SC-400 Exam Studies: Sensitivity Labels

---
+++ topics/technologies/microsoft_purview/PvPurview.psm1
@@ -0,0 +1,75 @@
+$run = '$main1'
+
+$main1 = {
+    New-PvLabel -DisplayName 'External'
+}
+
+$main2 = {
+
+}
+
+function Get-PvLabelPolicy {
+    param(
+        [switch]$TreeView
+    )
+    $labels = Get-Label | Select-Object Name, Priority, ExchangeObjectId,
+        @{n = 'LabelPath'; e = {
+            if ($_.ParentId) {
+                "$( $_.ParentLabelDisplayName ) > $( $_.DisplayName )"
+            }
+            else {
+                "$( $_.DisplayName )"
+            }
+        }}
+
+    $labelLookup = @{}
+    foreach ($l in $labels) {
+        $labelLookup[$l.Name] = $l
+    }
+
+    $results = Get-LabelPolicy -WarningAction Ignore | 
+        Select-Object Name, Priority, CreatedBy, WhenChanged, ExchangeObjectId,
+            @{n = 'Labels'; e = {
+                $sortedLabels = [System.Collections.Generic.List[PSCustomObject]]::new()
+                foreach ($l in $_.Labels) {
+                    if ($labelLookup.ContainsKey($l)) {
+                        $sortedLabels.Add(
+                            [PSCustomObject]@{
+                                Name = $l
+                                Priority = $labelLookup[$l].Priority
+                                LabelPath = $labelLookup[$l].LabelPath
+                            }
+                        )
+                    }
+                }
+                $sortedLabels | Sort-Object Priority | Select-Object -ExpandProperty LabelPath
+            }}
+        
+    if ($PSBoundParameters.TreeView) {
+        $results | Select-Object Name, 
+            @{n='Labels';e={$_.Labels -join "`n"}} |
+            Format-Table -wrap
+    } else {
+        $results
+    }
+}
+
+
+function New-PvLabel {
+    param(
+        [Parameter(Mandatory)]
+        [string]$DisplayName,
+        [string]$OrgPrefix = 'myorg',
+        [string]$Tooltip = 'TBD'
+    )
+    $guid = (New-Guid).ToString().Substring(0,8)
+    $labelName = "$OrgPrefix`_$guid"
+    try {
+        New-Label -DisplayName $DisplayName -Name $labelName -Tooltip $Tooltip -ErrorAction stop -WarningAction ignore
+    } catch {
+        Write-Warning $_.Exception.Message
+    }
+}
+
+$run = Invoke-Expression $run
+& $run
